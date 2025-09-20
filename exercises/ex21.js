189a82baa7902c915d4cac0da6a02abd89b8dd57
Leo <leozinho17silvacosta@gmail.com>
2025-09-19 21:06:26 -0700

    complete exercise 21

--- exercises/ex21.js
+++ exercises/ex21.js
@@ -6,3 +6,11 @@ TIP: The 1st element of any array has an index of 0.
 
 Afterwards, print out the prices array to the console.
 */
+
+//              0      1      2     3     4     5     6     7    
+let prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+prices[0] = 24.6;
+prices[2] = 45.93
+prices[6] = 34.46
+
+console.table(prices)
